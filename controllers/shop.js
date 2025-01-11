666b612f7393c84338992cae1ec1d392d7e3fea7
unknown <experte001@gmail.com>
2025-01-11 17:32:39 +0900

    Fetching Admin products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,13 +1,14 @@
 const Product = require("../models/product");
 const Cart = require("../models/cart");
+const { where } = require("sequelize");
 
 const getIndex = (req, res, next) => {
   Product.findAll()
     .then((products) => {
-      res.render("admin/products", {
+      res.render("shop/index", {
         prods: products,
         pageTitle: "Admin Products",
-        path: "/admin/products",
+        path: "/",
       });
     })
     .catch((err) => console.log(err));
@@ -27,15 +28,25 @@ const getProducts = (req, res, next) => {
 
 const getProduct = (req, res, next) => {
   const productId = req.params.productId;
-  Product.findById(productId)
-    .then(([product]) => {
+  Product.findAll({ where: { id: productId } })
+    .then((products) => {
       res.render("shop/product-detail", {
-        product: product[0],
-        pageTitle: product.title,
+        product: products[0],
+        pageTitle: products[0].title,
         path: "/products",
       });
     })
     .catch((err) => console.log(err));
+
+  // Product.findByPk(productId)
+  //   .then((product) => {
+  //     res.render("shop/product-detail", {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
 };
 
 const getCart = (req, res, next) => {
