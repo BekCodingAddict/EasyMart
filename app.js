e35ab3808c33bd4fa5fa0f452760e4345dafc960
unknown <experte001@gmail.com>
2025-01-11 19:27:33 +0900

    One to many & Many to Many Reletionship �

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorPage = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cartItem");
 
 const app = express();
-
+//express Setting
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
+//express using
 app.use((req, res, next) => {
   User.findByPk(1)
     .then((user) => {
@@ -34,9 +35,14 @@ app.use(errorPage);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
